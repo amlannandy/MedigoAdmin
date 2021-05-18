6d9e7ff1d46e271d91093bf990ad10e92dae6d43
Amlan <amlannandy5@gmail.com>
2021-05-18 21:28:25 +0530

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
 import './app.css';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
+import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
 
 const App = () => {
-  return <Login />;
+  return (
+    <Switch>
+      <Route path='/' exact component={Home} />
+      <Route path='/login' exact component={Login} />
+    </Switch>
+  );
 };
 
 export default App;
