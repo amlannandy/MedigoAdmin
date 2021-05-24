c0d777ee07cdcc2b921aac26df65c7a91fc300fa
Amlan <amlannandy5@gmail.com>
2021-05-24 22:11:10 +0530

    init profile page

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,9 +1,26 @@
+import './style.css';
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Profile = () => {
+  const { user, isLoading } = useSelector(state => state.auth);
+
+  if (isLoading) {
+    return <h1>Loading</h1>;
+  }
+
+  if (!user) {
+    return <h1>Profile not found</h1>;
+  }
+
   return (
-    <div>
-      <h1>Profile</h1>
+    <div className='container'>
+      <div className='upper-profile'>
+        <img src={user.imageUrl} alt='' className='profile-picture' />
+        <div className='upper-profile-right-section'>
+          <h3>{user.name}</h3>
+        </div>
+      </div>
     </div>
   );
 };
