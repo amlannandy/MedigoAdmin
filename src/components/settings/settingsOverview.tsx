d94051781e1c8d40751cd4c80632c23b3bf2b47a
Amlan <amlannandy5@gmail.com>
2021-08-18 13:11:06 +0530

    finished settings overview

--- src/components/settings/settingsOverview.tsx
+++ src/components/settings/settingsOverview.tsx
@@ -1,11 +1,46 @@
 import React from 'react';
+import { Link, RouteComponentProps } from 'react-router-dom';
+import { Header, List, Icon } from 'semantic-ui-react';
 
-export default class settingsOverview extends React.Component {
+class SettingsOverview extends React.Component<RouteComponentProps> {
   render() {
+    const { match } = this.props;
     return (
-      <div>
-        <h1>Settings Overview</h1>
-      </div>
+      <React.Fragment>
+        <Header as='h2' color='grey'>
+          <Icon name='settings' />
+          <Header.Content>
+            Account Settings
+            <Header.Subheader>Configure your account</Header.Subheader>
+          </Header.Content>
+        </Header>
+        <p>What would you like to change about your account?</p>
+        <List bulleted>
+          <List.Item
+            content={
+              <Link to={`${match.url}/change-email`}>
+                I want to change the email address associated with my account
+              </Link>
+            }
+          />
+          <List.Item
+            content={
+              <Link to={`${match.url}/update-password`}>
+                I want to update my password
+              </Link>
+            }
+          />
+          <List.Item
+            content={
+              <Link to={`${match.url}/delete-account`}>
+                I want to delete my account
+              </Link>
+            }
+          />
+        </List>
+      </React.Fragment>
     );
   }
 }
+
+export default SettingsOverview;
